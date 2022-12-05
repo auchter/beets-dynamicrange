62fc157f85293d1d2dcc36b5afa33d5322cc8c5f
Michael Auchter <a@phire.org>
2022-12-05 14:41:38 +0000

    fix indentation

--- flake.nix
+++ flake.nix
@@ -11,37 +11,36 @@
     devShell = pkgs.mkShell {
       buildInputs = with pkgs; [
         self.defaultPackage."${system}"
-        ];
-        };
+      ];
+    };
 
-        defaultPackage = pkgs.python3Packages.buildPythonApplication rec {
-          pname = "beets-dynamicrange";
-          version = "unstable-2022-08-15";
+    defaultPackage = pkgs.python3Packages.buildPythonApplication rec {
+      pname = "beets-dynamicrange";
+      version = "unstable-2022-08-15";
 
-          src = ./.;
+      src = ./.;
 
-          nativeBuildInputs = [ pkgs.beets ];
-          propagatedBuildInputs = [ pkgs.dr14_tmeter ];
+      nativeBuildInputs = [ pkgs.beets ];
+      propagatedBuildInputs = [ pkgs.dr14_tmeter ];
 
-          postPatch = ''
-            substituteInPlace beetsplug/dynamicrange.py \
-              --replace dr14_tmeter ${pkgs.dr14_tmeter}/bin/dr14_tmeter
-          '';
+      postPatch = ''
+        substituteInPlace beetsplug/dynamicrange.py \
+          --replace dr14_tmeter ${pkgs.dr14_tmeter}/bin/dr14_tmeter
+      '';
 
-          doCheck = false;
+      doCheck = false;
 
-          meta = with pkgs.lib; {
-            homepage = "https://github.com/auchter/beets-dynamicrange";
-            description = "Calculate and store dynamic range of music for beets";
-            license = licenses.mit;
-            maintainers = with maintainers; [ auchter ];
-            platforms = platforms.linux;
-          };
-        };
+      meta = with pkgs.lib; {
+        homepage = "https://github.com/auchter/beets-dynamicrange";
+        description = "Calculate and store dynamic range of music for beets";
+        license = licenses.mit;
+        maintainers = with maintainers; [ auchter ];
+        platforms = platforms.linux;
+      };
+    };
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
-
-      }; in with utils.lib; eachSystem defaultSystems out;
-    }
+    defaultApp = utils.lib.mkApp {
+      drv = self.defaultPackage."${system}";
+    };
+  }; in with utils.lib; eachSystem defaultSystems out;
+}
